1578c115106f8181eec0c58396a06db7a4165e88
Allan <allan.j@bealy.io>
2023-10-16 21:20:22 +0200

    Export logic to class

--- server/src/Laposte/controllers/getAllCities.controller.ts
+++ server/src/Laposte/controllers/getAllCities.controller.ts
@@ -1,19 +1,9 @@
-import fs from 'fs';
 import { Request, Response } from 'express';
-import { IDatabase } from '../../Services/database/entities';
+import LaPoste from '../businessLogic/laposte';
 
 const getAllCities = async (req: Request, res: Response) => {
     try {
-        fs.readFile('./src/Services/database/laposte_hexasmal.json', 'utf8', (err, data) => {
-            if (err) {
-                console.error(err);
-                return res.status(500).send('Erreur lors de la lecture du fichier JSON');
-            }
-
-            const jsonData = JSON.parse(data);
-            const allCities = jsonData.map((item: IDatabase) => item.fields.nom_de_la_commune);
-            return res.status(200).json({ data: allCities, status: 200 })
-        });
+        return res.status(200).json({ data: LaPoste.getAllCities(), status: 200 })
     } catch (err) {
         return res.status(500).json({ message: "Internal error sorry", status: 500 })
     }
